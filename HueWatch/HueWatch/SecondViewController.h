bc0fba9ff1a65a04feea67f7346b0edeb768749e
Jordan Zucker <jordan.zucker@gmail.com>
2015-05-02 15:45:54 -0700

    Basic project up and running with Podfile

---
+++ HueWatch/HueWatch/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  HueWatch
+//
+//  Created by Jordan Zucker on 5/2/15.
+//  Copyright (c) 2015 Jordan Zucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
